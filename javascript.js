55265438fdf768fa788057e5a6dadc920ab7ab02
rutayisirerolando <rutayisirerolando@gmail.com>
2025-05-17 16:36:10 +0200

    second commit

--- javascript.js
+++ javascript.js
@@ -8,3 +8,23 @@ console.log(laterYears);
 let myAgeInDogYears = earlyYears+laterYears;
 let myName = "rolando".toLowerCase();
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years. `);
+let sale = true;
+sale = false;
+if (sale) {
+    console.log("Time to buy!");
+} else {
+    console.log("Time to wait for a sale.");
+}
+let hungerLevel = 7;
+if (hungerLevel>7) {
+    comsole.log("Time to eat!");
+} else {
+    console.log("We can eat later!");
+}
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel>8){
+  console.log("time to sleep")
+} else {
+  console.log("not bed time yet")
+}
